233f676f9634caa38ac471d8f957a5891a2410f9
Laffery <2387065420@qq.com>
2021-03-23 19:16:50 +0800

    :zap: server ok

---
+++ lesson-3-homework/Spider/server.js
@@ -0,0 +1,49 @@
+import { first, next, log } from './spider.js';
+import {
+    createRequire
+} from "module";
+const require = createRequire(
+    import.meta.url);
+import {
+    stringify
+} from "node:querystring";
+let fs = require("fs");
+
+var http = require("http");
+var url = require("url");
+var hostname = 'localhost';
+var port = 2021;
+ 
+function start() {
+    function onRequest(req, res) {
+        var pathname = url.parse(req.url).pathname;
+        log("Request for " + pathname + " received.");
+        
+        if (pathname === '/home') {
+            first();
+            res.writeHead(200, {"Content-Type": "text/json"});
+            res.end(fs.readFileSync('res.json'));
+        }
+        else if (pathname === '/more') {
+            next();
+            res.writeHead(200, {"Content-Type": "text/json"});
+            res.end(fs.readFileSync('res.json'));
+        }
+        else if (pathname === '/') {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Hello world');
+        }
+        else {
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end('Bad Request.');
+            log('Bad Request.');
+        }
+    }
+ 
+    http.createServer(onRequest).listen(port);
+    log(`Server running at http://${hostname}:${port}/`);
+}
+ 
+// start();
+
+// export { start }
--- lesson-3-homework/Spider/spider.js
+++ lesson-3-homework/Spider/spider.js
@@ -10,16 +10,15 @@ import {
 let fs = require("fs");
 import { _signature } from './acrawler.js';
 
-var _url = 'https://www.toutiao.com/api/pc/feed/?min_behot_time=0&category=__all__&utm_source=toutiao&widen=1&tadrequire=true';
-
-var url = _url + '&_signature=' + _signature(_url)
-
 function log(msg) {
     var d = new Date();
-    console.log(d.toUTCString() + ': ' + msg);
+    console.log(d.toISOString() + ': ' + msg);
 }
 
-function batch() {
+function first() {
+    var _url = 'https://www.toutiao.com/api/pc/feed/?min_behot_time=0&category=__all__&utm_source=toutiao&widen=1&tadrequire=true';
+    var url = _url + '&_signature=' + _signature(_url)
+    
     axios
     .get(url, {
         headers: {
@@ -28,27 +27,36 @@ function batch() {
     })
     .then((res) => {
         fs.writeFileSync('res.json', JSON.stringify(res.data));
-        log('spider success!');
-        console.log(res.data.next.max_behot_time);
-        return res.data.next.max_behot_time;
+        log('SUCCESS ' + url);
     })
     .catch(() => {
-        log('spider get error');
+        log('ERROR ' + url);
     });
 }
 
-log(batch())
+// first();
 
-// const http = require('http');
-// const hostname = 'localhost';
-// const port = 2021;
-// const server = http.createServer((req, res) => {
-//     console.log(batch());
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/json');
-//     var data = fs.readFileSync('res.json');
-//     res.end(data);
-// });
-// server.listen(port, hostname, () => {
-//     log(`Server running at http://${hostname}:${port}/`);
-// });
+function next() {
+    var last = JSON.parse(fs.readFileSync('res.json'));
+    var max_behot_time = last.next.max_behot_time;
+
+    var _url = 'https://www.toutiao.com/api/pc/feed/?max_behot_time=' + max_behot_time + '&category=__all__&utm_source=toutiao&widen=1&tadrequire=true';
+    var url = _url + '&_signature=' + _signature(_url);
+    axios
+    .get(url, {
+        headers: {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4442.4 Safari/537.36",
+        },
+    })
+    .then((res) => {
+        fs.writeFileSync('res.json', JSON.stringify(res.data));
+        log('SUCCESS ' + url);
+    })
+    .catch(() => {
+        log('ERROR' + url);
+    });
+}
+
+// next();
+
+export { first, next, log };
